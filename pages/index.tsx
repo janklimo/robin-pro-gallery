84d73d84ad87d5a88c0a7d90ac1a7873750e36f2
Jan Klimo <jan.klimo@gmail.com>
2023-03-21 22:53:54 +0700

    Copy and images

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,11 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { useState } from "react";
 
+import image1 from "public/image1.png";
+import image2 from "public/image2.png";
+import image3 from "public/image3.png";
+import image4 from "public/image4.png";
+import image5 from "public/image5.png";
 import logo from "public/logo.png";
 import Head from "../components/Head";
 
@@ -183,14 +188,17 @@ export default function Home() {
                 <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                   <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                      We’re changing the way people connect.
+                      Shopify photo gallery app built for{" "}
+                      {new Date().getFullYear()}
                     </h1>
                     <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                      Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-                      quis cupidatat mollit aute velit. Et labore commodo nulla
-                      aliqua proident mollit ullamco exercitation tempor. Sint
-                      aliqua anim nulla sunt mollit id pariatur in voluptate
-                      cillum.
+                      With Robin PRO you can easily create beautiful, fast,
+                      mobile-friendly photo galleries, protect them with
+                      watermarks and link them to products to drive sales.
+                      Design pixel-perfect grid and masonry layouts. Let it
+                      optimize and lazy-load your images in the perfect size and
+                      format from a fast CDN. Upload in bulk and publish in
+                      seconds.
                     </p>
                     <div className="mt-10 flex items-center gap-x-6">
                       <a
@@ -205,46 +213,71 @@ export default function Home() {
                   <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                     <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                       <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
+                        <Image
+                          src={image1}
+                          layout="responsive"
+                          placeholder="blur"
+                          width={500}
+                          height={750}
                           className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                          alt="A bouquet of flowers"
+                          title="A bouquet of flowers"
                         />
                         <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                       </div>
                     </div>
                     <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                       <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
+                        <Image
+                          src={image2}
+                          layout="responsive"
+                          placeholder="blur"
+                          width={500}
+                          height={750}
                           className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                          alt="A bouquet of flowers"
+                          title="A bouquet of flowers"
                         />
                         <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                       </div>
                       <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
-                          alt=""
+                        <Image
+                          src={image3}
+                          layout="responsive"
+                          placeholder="blur"
+                          width={500}
+                          height={749}
                           className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                          alt="A bouquet of flowers"
+                          title="A bouquet of flowers"
                         />
                         <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                       </div>
                     </div>
                     <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                       <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
-                          alt=""
+                        <Image
+                          src={image4}
+                          layout="responsive"
+                          placeholder="blur"
+                          width={500}
+                          height={750}
                           className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                          alt="A bouquet of flowers"
+                          title="A bouquet of flowers"
                         />
                         <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                       </div>
                       <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
+                        <Image
+                          src={image5}
+                          layout="responsive"
+                          placeholder="blur"
+                          width={500}
+                          height={667}
                           className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                          alt="Flower shop"
+                          title="Flower shop"
                         />
                         <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                       </div>
